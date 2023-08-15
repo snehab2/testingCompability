063dbea6c3f1f0175d4465d3900c8e3eda1a6b0a
Sneha Bista <snehab@prodigy.net>
2023-08-15 12:24:17 -0500

    Initial Commit

---
+++ testingCompability/testingCompabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingCompabilityApp.swift
+//  testingCompability
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct testingCompabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
